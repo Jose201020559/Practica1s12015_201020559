f525721337da95e909a99ace949d1a931fee7df2
Jose <Jose@Jose-PC>
2015-02-13 17:57:44 -0600

    new commit

---
+++ src/practica1s12015_201020559/Practica1s12015_201020559.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practica1s12015_201020559;
+
+/**
+ *
+ * @author Jose
+ */
+public class Practica1s12015_201020559 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
