1d4af43d588d0405efff814cf84432e7b4a4363b
Jose <Jose@Jose-PC>
2015-02-16 21:51:03 -0600

    LISTA JUGADORES-DATOS

--- src/practica1s12015_201020559/Practica1s12015_201020559.java
+++ src/practica1s12015_201020559/Practica1s12015_201020559.java
@@ -6,6 +6,8 @@
 
 package practica1s12015_201020559;
 
+import java.io.IOException;
+
 /**
  *
  * @author Jose
@@ -15,8 +17,26 @@ public class Practica1s12015_201020559 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        
+        ListaJugadores jugadores = new ListaJugadores();
+        jugadores.insertarJ("PLANTA");
+        jugadores.insertarJ("ZOMBI");
+        jugadores.insertarJ ("NADA");
+        
+        jugadores.InsertarDato(jugadores.buscar("ZOMBI"),"Jose");
+        jugadores.InsertarDato(jugadores.buscar("ZOMBI"),"Pablo");
+        jugadores.InsertarDato(jugadores.buscar("ZOMBI"),"Araiz");
+        jugadores.InsertarDato(jugadores.buscar("ZOMBI"),"Lopez");
+        
+        jugadores.InsertarDato(jugadores.buscar("PLANTA"),"SEBAS");
+        jugadores.InsertarDato(jugadores.buscar("PLANTA"),"ALEX");
+        jugadores.InsertarDato(jugadores.buscar("PLANTA"),"LOPEZ");
+        jugadores.InsertarDato(jugadores.buscar("PLANTA"),"CALDERON");
+        
+        jugadores.imprimir();
+        jugadores.GraficarLista();
     }
     
 }
